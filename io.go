5316b6c773413aa123673d37f51611d2e31a209c
Adam Brown <adamwbrown@gmail.com>
2021-05-17 00:24:32 -0700

    Reorganized

---
+++ io.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"minetally/api"
+	"os"
+)
+
+func RenderConfig(file string) (Config, error) {
+	var parsed Config
+	cfgFile, err := os.Open(file)
+
+	if err != nil {
+		LogError.Printf("Error loading config: %s", file)
+		return parsed, err
+	}
+	defer cfgFile.Close()
+
+	parser := json.NewDecoder(cfgFile)
+	err = parser.Decode(&parsed)
+
+	return parsed, err
+}
+
+func getHomeDir() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		LogError.Fatal("failed to get home directory")
+	}
+
+	return home
+}
+
+func getConfigDir() string {
+	home := getHomeDir()
+	return fmt.Sprintf("%s/.minetally/", home)
+}
+
+const configFileName = "tally.json"
+const jsonDataFileName = "data.json"
+
+func makeHomeDir(configFileDir string) {
+	// Make config dir if it doesn't exist
+	if _, err := os.Stat(configFileDir); os.IsNotExist(err) {
+		err = os.Mkdir(configFileDir, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func createConfig(configFileDir string, configFileName string) Config {
+
+	fmt.Println("Enter Wallet Address: (0x000000000000000000000000000)")
+	var walletAddress string
+	_, err := fmt.Scanln(&walletAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	newConfig := Config{
+		WalletAddress: walletAddress,
+		Users:         []User{},
+	}
+
+	configJson, err := json.Marshal(newConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	makeHomeDir(configFileDir)
+
+	configFilePath := configFileDir + configFileName
+
+	writeStringToFile(configJson, configFilePath)
+
+	return newConfig
+}
+
+func saveData() {
+	workerData := WorkerData{
+		Workers: []api.Worker{},
+		Shares:  make(map[int]map[int]int),
+	}
+
+	for worker, shares := range Workers {
+		workerData.Workers = append(workerData.Workers, worker)
+
+		for date, share := range shares {
+			if workerData.Shares[worker.UID] == nil {
+				workerData.Shares[worker.UID] = make(map[int]int)
+			}
+
+			workerData.Shares[worker.UID][date] = share
+		}
+	}
+
+	workerJsonData, err := json.Marshal(workerData)
+	if err != nil {
+		panic(err)
+	}
+
+	configDir := getConfigDir()
+	dataFile := configDir + jsonDataFileName
+
+	writeStringToFile(workerJsonData, dataFile)
+
+	LogInfo.Println("Data saved to " + dataFile)
+}
+
+func writeStringToFile(data []byte, filePath string) {
+	fh, err := os.Create(filePath)
+	if err != nil {
+		LogError.Fatal(err)
+	}
+
+	_, err = fh.Write(data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readData() {
+	configDir := getConfigDir()
+	dataFile := configDir + jsonDataFileName
+
+	if _, err := os.Stat(dataFile); os.IsExist(err) {
+		LogInfo.Println("Data file exists, reading it into memory")
+
+		// Read data from the file
+		data, err := ioutil.ReadFile(dataFile)
+		if err != nil {
+			LogError.Fatal(err)
+		}
+
+		var workerData WorkerData
+
+		// Unmarshall it
+		err = json.Unmarshal(data, &workerData)
+		if err != nil {
+			LogError.Fatal(err)
+		}
+
+		// Fill our in-memory data structure with it
+		for _, worker := range workerData.Workers {
+			Workers[worker] = make(map[int]int)
+		}
+
+		// Fill the shares for each worker from the stored data
+		for workerUid, shares := range workerData.Shares {
+			worker, err := findWorkerForUid(workerUid)
+			if err != nil {
+				panic(err)
+			}
+
+			Workers[worker] = make(map[int]int)
+			for date, share := range shares {
+				Workers[worker][date] = share
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -70,40 +69,7 @@ func ConfigureLogging(debug bool, w io.Writer) {
 	}
 }
 
-func RenderConfig(file string) (Config, error) {
-	var parsed Config
-	cfgFile, err := os.Open(file)
-
-	if err != nil {
-		LogError.Printf("Error loading config: %s", file)
-		return parsed, err
-	}
-	defer cfgFile.Close()
-
-	parser := json.NewDecoder(cfgFile)
-	err = parser.Decode(&parsed)
-
-	return parsed, err
-}
-
 const PollInterval = 10 * time.Second
-
-func getHomeDir() string {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		LogError.Fatal("failed to get home directory")
-	}
-
-	return home
-}
-
-func getConfigDir() string {
-	home := getHomeDir()
-	return fmt.Sprintf("%s/.minetally/", home)
-}
-
-const configFileName = "tally.json"
-const jsonDataFileName = "data.json"
 
 func main() {
 	var err error
@@ -143,44 +109,6 @@ func main() {
 		//debug_printShares()
 		time.Sleep(PollInterval)
 	}
-}
-
-func makeHomeDir(configFileDir string) {
-	// Make config dir if it doesn't exist
-	if _, err := os.Stat(configFileDir); os.IsNotExist(err) {
-		err = os.Mkdir(configFileDir, 0755)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func createConfig(configFileDir string, configFileName string) Config {
-
-	fmt.Println("Enter Wallet Address: (0x000000000000000000000000000)")
-	var walletAddress string
-	_, err := fmt.Scanln(&walletAddress)
-	if err != nil {
-		panic(err)
-	}
-
-	newConfig := Config{
-		WalletAddress: walletAddress,
-		Users:         []User{},
-	}
-
-	configJson, err := json.Marshal(newConfig)
-	if err != nil {
-		panic(err)
-	}
-
-	makeHomeDir(configFileDir)
-
-	configFilePath := configFileDir + configFileName
-
-	writeStringToFile(configJson, configFilePath)
-
-	return newConfig
 }
 
 func pollForWorkers() {
@@ -236,90 +164,6 @@ func userForWorker(worker api.Worker) (User, error) {
 	}
 	return foundUser, fmt.Errorf("user not found for worker '%s'", worker.ID)
 
-}
-
-func saveData() {
-	workerData := WorkerData{
-		Workers: []api.Worker{},
-		Shares:  make(map[int]map[int]int),
-	}
-
-	for worker, shares := range Workers {
-		workerData.Workers = append(workerData.Workers, worker)
-
-		for date, share := range shares {
-			if workerData.Shares[worker.UID] == nil {
-				workerData.Shares[worker.UID] = make(map[int]int)
-			}
-
-			workerData.Shares[worker.UID][date] = share
-		}
-	}
-
-	workerJsonData, err := json.Marshal(workerData)
-	if err != nil {
-		panic(err)
-	}
-
-	configDir := getConfigDir()
-	dataFile := configDir + jsonDataFileName
-
-	writeStringToFile(workerJsonData, dataFile)
-
-	LogInfo.Println("Data saved to " + dataFile)
-}
-
-func writeStringToFile(data []byte, filePath string) {
-	fh, err := os.Create(filePath)
-	if err != nil {
-		LogError.Fatal(err)
-	}
-
-	_, err = fh.Write(data)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func readData() {
-	configDir := getConfigDir()
-	dataFile := configDir + jsonDataFileName
-
-	if _, err := os.Stat(dataFile); os.IsExist(err) {
-		LogInfo.Println("Data file exists, reading it into memory")
-
-		// Read data from the file
-		data, err := ioutil.ReadFile(dataFile)
-		if err != nil {
-			LogError.Fatal(err)
-		}
-
-		var workerData WorkerData
-
-		// Unmarshall it
-		err = json.Unmarshal(data, &workerData)
-		if err != nil {
-			LogError.Fatal(err)
-		}
-
-		// Fill our in-memory data structure with it
-		for _, worker := range workerData.Workers {
-			Workers[worker] = make(map[int]int)
-		}
-
-		// Fill the shares for each worker from the stored data
-		for workerUid, shares := range workerData.Shares {
-			worker, err := findWorkerForUid(workerUid)
-			if err != nil {
-				panic(err)
-			}
-
-			Workers[worker] = make(map[int]int)
-			for date, share := range shares {
-				Workers[worker][date] = share
-			}
-		}
-	}
 }
 
 func findWorkerForUid(uid int) (api.Worker, error) {
